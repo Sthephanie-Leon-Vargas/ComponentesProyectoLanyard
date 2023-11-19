540cffd11e3b2eff9c005776b448957a12f980e7
Gia Montero <gmonteroq@ucenfotec.ac.cr>
2023-11-18 19:50:17 -0600

    Arreglo de html

--- src/main/java/com/componentes/landyard/controller/IngresarController.java
+++ src/main/java/com/componentes/landyard/controller/IngresarController.java
@@ -1,15 +1,18 @@
 package com.componentes.landyard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
-
 @Controller
-@RequestMapping("/register")
-public class RegisterController {
+@RequestMapping("/ingresar")
+public class IngresarController {
+
     @GetMapping("")
-    public String register(Model model){
-        return "register";
+    public String Ingresar(Model model){
+
+        return "ingresar";
     }
-}
+
+
+    }
