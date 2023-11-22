66ac75cfc5b42f95afc0e384795fda3026b4acc9
mruizo <mruizo@ucenfotec.ac.cr>
2023-11-22 17:06:59 -0600

    Rename Home to Index.

--- src/main/java/com/componentes/landyard/LandyardApplication.java
+++ src/main/java/com/componentes/landyard/LandyardApplication.java
@@ -1,5 +1,4 @@
 package com.componentes.landyard;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/componentes/landyard/controller/HomeController.java
+++ src/main/java/com/componentes/landyard/controller/HomeController.java
@@ -1,5 +1,4 @@
 package com.componentes.landyard.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,10 +7,11 @@ import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
-    @GetMapping("/home")
-    public String home(@RequestParam(name= "nombre", required = false, defaultValue = "Gia Virtual") String name, Model model){
+
+    @GetMapping("/index")
+    public String index (@RequestParam(name= "nombre", required = false, defaultValue = "Gia Virtual") String name, Model model){
         model.addAttribute("nombre",name);
-        return "home";
+        return "index";
     }
     @GetMapping("/singleBlog")
     public String singleBlog(@RequestParam(name= "nombre", required = false, defaultValue = "Gia Virtual") String name, Model model){
